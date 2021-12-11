12b22b9f3f0ec4e1d563f5c532a111b440a60594
bsergiov <81264661+bsergiov@users.noreply.github.com>
2021-12-11 10:08:29 +0200

    Initial Commit

---
+++ sliderRgb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sliderRgb
+//
+//  Created by Сергей Бровко on 11.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
